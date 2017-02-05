196e9cf93d1642a133212b39ef9000e42bc544fb
danteskin <buck2192@gmail.com>
2017-02-05 16:48:54 +0500

    [bugfix] header-icon - change display flex on table

--- bitrix/templates/Holiday/include_areas/header_icons.php
+++ bitrix/templates/Holiday/include_areas/header_icons.php
@@ -5,9 +5,9 @@ $page_a = explode("/",$page);
 //echo $page_a[1];
 ?>
 
-							<li class="item_5 <?=($page_a[1]=="sibiriada") ?'active':'';?>"><a href="/sibiriada"><img src="<?=$APPLICATION->GetTemplatePath("")?>img/logo_sibiriada.svg" alt=""></a></li>
-							<li class="item_4 <?=($page_a[1]=="holiday-super") ?'active':'';?>"><a href="/holiday-super"><img src="<?=$APPLICATION->GetTemplatePath("")?>img/logo_super.svg" alt=""></a></li>
-							<li class="item_3 <?=($page_a[1]=="planeta-holiday") ?'active':'';?>"><a href="/planeta-holiday"><img src="<?=$APPLICATION->GetTemplatePath("")?>img/logo_planet.svg" alt=""></a></li>
-							<li class="item_2 <?=($page_a[1]=="kora") ?'active':'';?>"><a href="/kora"><img src="<?=$APPLICATION->GetTemplatePath("")?>img/logo_kopa.svg" alt=""></a></li>
-							<li class="item_1 <?=($page_a[1]=="") ?'active':'';?>"><a href="/"><img src="<?=$APPLICATION->GetTemplatePath("")?>img/logo.svg" alt=""></a></li>
+							<li class="item_5 <?=($page_a[1]=="sibiriada") ?'active':'';?>"><a class="link__city" href="/sibiriada"><div class="item__inner"><img src="<?=$APPLICATION->GetTemplatePath("")?>img/logo_sibiriada.svg" alt=""></div></a></li>
+							<li class="item_4 <?=($page_a[1]=="holiday-super") ?'active':'';?>"><a class="link__city" href="/holiday-super"><div class="item__inner"><img src="<?=$APPLICATION->GetTemplatePath("")?>img/logo_super.svg" alt=""></div></a></li>
+							<li class="item_3 <?=($page_a[1]=="planeta-holiday") ?'active':'';?>"><a class="link__city" href="/planeta-holiday"><div class="item__inner"><img src="<?=$APPLICATION->GetTemplatePath("")?>img/logo_planet.svg" alt=""></div></a></li>
+							<li class="item_2 <?=($page_a[1]=="kora") ?'active':'';?>"><a class="link__city" href="/kora"><div class="item__inner"><img src="<?=$APPLICATION->GetTemplatePath("")?>img/logo_kopa.svg" alt=""></div></a></li>
+							<li class="item_1 <?=($page_a[1]=="") ?'active':'';?>"><a class="link__city" href="/"><div class="item__inner"><img src="<?=$APPLICATION->GetTemplatePath("")?>img/logo.svg" alt=""></div></a></li>
 						
