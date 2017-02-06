b226b991c1453256993554f93e77613744f6e19b
danteskin <buck2192@gmail.com>
2017-02-06 14:58:09 +0500

    [addFilter for map with diffrent label]

--- bitrix/templates/Holiday/include_areas/header_icons.php
+++ bitrix/templates/Holiday/include_areas/header_icons.php
@@ -6,34 +6,34 @@ $page_a = explode("/",$page);
 ?>
 
 							<li class="item_5 <?=($page_a[1]=="sibiriada") ?'active':'';?>">
-                <a class="link__city" href="/sibiriada" data-cookie="Сибириада">
+                <a class="link__city" href="/sibiriada" data-cookie="sibiriada">
                   <div class="item__inner">
                     <img src="<?=$APPLICATION->GetTemplatePath("")?>img/logo_sibiriada.svg" alt="">
                   </div>
                 </a>
               </li>
 							<li class="item_4 <?=($page_a[1]=="holiday-super") ?'active':'';?>">
-                <a class="link__city" href="/holiday-super" data-cookie="супер холидей">
+                <a class="link__city" href="/holiday-super" data-cookie="holidaySuper">
                   <div class="item__inner">
                     <img src="<?=$APPLICATION->GetTemplatePath("")?>img/logo_super.svg" alt="">
                   </div>
                 </a>
               </li>
 							<li class="item_3 <?=($page_a[1]=="planeta-holiday") ?'active':'';?>">
-                <a class="link__city" href="/planeta-holiday" data-cookie="Планета Холидей"><div class="item__inner">
+                <a class="link__city" href="/planeta-holiday" data-cookie="holidayPlanet"><div class="item__inner">
                     <img src="<?=$APPLICATION->GetTemplatePath("")?>img/logo_planet.svg" alt="">
                   </div>
                 </a>
               </li>
 							<li class="item_2 <?=($page_a[1]=="kora") ?'active':'';?>">
-                <a class="link__city" href="/kora" data-cookie="Кора">
+                <a class="link__city" href="/kora" data-cookie="kora">
                   <div class="item__inner">
                     <img src="<?=$APPLICATION->GetTemplatePath("")?>img/logo_kopa.svg" alt="">
                   </div>
                 </a>
               </li>
 							<li class="item_1 <?=($page_a[1]=="") ?'active':'';?>">
-                <a class="link__city" href="/" data-cookie="Холидей Классик">
+                <a class="link__city" href="/" data-cookie="holidayClassic">
                   <div class="item__inner">
                     <img src="<?=$APPLICATION->GetTemplatePath("")?>img/logo.svg" alt="">
                   </div>
@@ -41,9 +41,11 @@ $page_a = explode("/",$page);
               </li>
 
 <script type="text/javascript">
+  
 
   $('ul.stores li > a').click(function(e){
-    e.preventDefault();
+    
+
 
     // console.log($(this).data('cookie'));
     $.cookie('click', $(this).data('cookie'), { path: '/' });
