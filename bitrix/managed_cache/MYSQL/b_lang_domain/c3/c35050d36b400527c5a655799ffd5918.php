f51afb752c491c1ba0b1305a0fbd7323748066c7
Blackidea <master@blackidea.ru>
2017-02-06 18:47:49 +0300

    commit

--- bitrix/managed_cache/MYSQL/b_lang_domain/c3/c35050d36b400527c5a655799ffd5918.php
+++ bitrix/managed_cache/MYSQL/b_lang_domain/c3/c35050d36b400527c5a655799ffd5918.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001486291745';
-$dateexpire = '001486295345';
+$datecreate = '001486395827';
+$dateexpire = '001486399427';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:0:{}}';
 return true;
 ?>
